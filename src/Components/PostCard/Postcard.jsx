6dcb2be024c55b7c9a39bda3091d759243b37a0e
AnuragBaruah47 <anuragbaruah2004@gmail.com>
2025-07-20 16:21:01 +0530

    Post is visible now but img is not

--- src/Components/PostCard/Postcard.jsx
+++ src/Components/PostCard/Postcard.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import appWriteService from "../../AppWrite/Conf";
 import { Link } from "react-router-dom";
 
-function Postcard({ $id, title, featuredImage }) {
+function Postcard({post}) {
+  const {$id,Title,Featuredimage}=post
   return (
     <Link to={`/post/${$id}`}>
       <div className="w-full bg-gray-100 rounded-xl p-4 ">
         <div className="w-full justify-center mb-4">
-          <img
-            src={appWriteService.getFilePreview(featuredImage)}
-            alt={title}
-            className="rounded-xl"
-          />
-          <h2 className="text-xl font-bold">{title}</h2>
+          {Featuredimage && (
+            <img
+              src={appWriteService.getFilePreview(Featuredimage)}
+              alt={Title}
+              className="rounded-xl"
+            />
+          )}
+          <h2 className="text-xl font-bold">{Title}</h2>
         </div>
       </div>
     </Link>
